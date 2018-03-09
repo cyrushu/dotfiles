c39669c59acd14ee8920cf774bb1068d52f87dd4
胡亦朗 <yilang@huyilangs-MacBook-Air.local>
2018-03-09 14:17:18 +0800

    new init_my_system

--- init_my_system.sh
+++ init_my_system.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-for i in "git-completion.bash" "tmux.conf" "vim" "vimrc" "zsh_env" "zshrc";
+the_dir=$(dirname $(pwd)/$0)
+
+ln_files=("git-completion.bash" "tmux.conf" "vim" "vimrc" "zsh_env" "zshrc")
+
+for i in ln_files;
 do
-	ln -s $(pwd)/$i ~/.$i
+	ln -s ${the_dir}/${i} ~/.${i}
 done
 
 cd ~
-mkdir .vimtmp
-mkdir .vimswap
-mkdir .vimundo
+# vim
+mkdir -p ~/.vimtmp
+mkdir -p ~/.vimswap
+mkdir -p ~/.vimundo
+
+# neovim
+mkdir -p ~/.config/
+ln -s ${the_dir}/nvim ~/.config/
